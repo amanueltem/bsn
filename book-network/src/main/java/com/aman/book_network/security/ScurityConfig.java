f8b809d154142d3e36d5155bccc49737e2063df9
Amanuel Temesgen <amanueltemesgen360@gmail.com>
2025-05-01 18:50:01 +0300

    forntend project intialized

--- book-network/src/main/java/com/aman/book_network/security/ScurityConfig.java
+++ book-network/src/main/java/com/aman/book_network/security/ScurityConfig.java
@@ -28,19 +28,18 @@ public class ScurityConfig {
                   cors(Customizer.withDefaults())
                   .csrf(AbstractHttpConfigurer::disable)
                   .authorizeHttpRequests(req->
-                  req.requestMatchers("auth/**"
-                                      ,"/v2/api-docs"
-                                      ,"/v3/api-docs"
-                                      ,"/v3/api-docs/**"
-                                      ,"/swagger-resources"
-                                      ,"/swagger-resources/**"
-                                      ,"/configuration/ui"
-                                      ,"/configuration/security"
-                                      ,"/swagger-ui/**"
-                                      ,"/webjars/**"
-                                      ,"/swagger-ui.html"
-                                      )
-                  .permitAll()
+                  req.requestMatchers(
+    "/auth/**",
+    "/v3/api-docs",
+    "/v3/api-docs/**",
+    "/swagger-resources",
+    "/swagger-resources/**",
+    "/configuration/ui",
+    "/configuration/security",
+    "/swagger-ui/**",
+    "/webjars/**",
+    "/swagger-ui.html"
+)                  .permitAll()
                   .anyRequest()
                   .authenticated()
                   )
